170e798d87abb8658594968176f95117cbe1e7a9
Nighty39 <134146343+Nighty39@users.noreply.github.com>
2024-10-26 14:03:15 +0200

    Update to 1.21.3

--- src/client/java/volery/macebut3d/mixin/client/ItemRendererAccessor.java
+++ src/client/java/volery/macebut3d/mixin/client/ItemRendererAccessor.java
@@ -1,12 +1,12 @@
 package volery.macebut3d.mixin.client;
 
-import net.minecraft.client.render.item.ItemModels;
 import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModelManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(ItemRenderer.class)
 public interface ItemRendererAccessor {
-    @Accessor("models")
-    ItemModels macebut3d$getModels();
+    @Accessor("bakedModelManager")
+    BakedModelManager macebut3d$getBakedModelManager();
 }
